19ba5aaa44e30e2963ff5d50b868e7b60fb52544
Carlos Colaço <coolapso@coolapso.tech>
2025-09-27 16:55:07 +0200

    Use sqlite database for persistent cache

--- go.mod
+++ go.mod
@@ -4,7 +4,8 @@ go 1.25.1
 
 require (
 	fyne.io/fyne/v2 v2.6.3
-	github.com/coolapso/go-utils v1.1.0
+	github.com/mattn/go-sqlite3 v1.14.32
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
 )
 
 require (
@@ -27,7 +28,6 @@ require (
 	github.com/jeandeaual/go-locale v0.0.0-20250612000132-0ef82f21eade // indirect
 	github.com/jsummers/gobmp v0.0.0-20230614200233-a9de23ed2e25 // indirect
 	github.com/kr/text v0.2.0 // indirect
-	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
 	github.com/nicksnyder/go-i18n/v2 v2.5.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rymdport/portal v0.4.1 // indirect
---
+++ internal/database/db.go
@@ -0,0 +1,108 @@
+package database
+
+import (
+	"bytes"
+	"database/sql"
+	"image"
+	"image/jpeg"
+	"log"
+	"path/filepath"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+const (
+	currentSchemaVersion = 1
+	dbFileName           = ".picsort.db"
+)
+
+type DB struct {
+	conn *sql.DB
+}
+
+func New(datasetPath string) (*DB, error) {
+	dbPath := filepath.Join(datasetPath, dbFileName)
+	conn, err := sql.Open("sqlite3", dbPath)
+	if err != nil {
+		return nil, err
+	}
+
+	db := &DB{conn: conn}
+	if err := db.migrate(); err != nil {
+		conn.Close()
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func (db *DB) Close() {
+	db.conn.Close()
+}
+
+func (db *DB) migrate() error {
+	_, err := db.conn.Exec(`
+		CREATE TABLE IF NOT EXISTS metadata (
+			key TEXT PRIMARY KEY,
+			value TEXT
+		);
+		CREATE TABLE IF NOT EXISTS thumbnails (
+			path TEXT PRIMARY KEY,
+			data BLOB
+		);
+		CREATE TABLE IF NOT EXISTS images (
+			path TEXT PRIMERY KEY,
+			bin INTEGER
+		);
+	`)
+	if err != nil {
+		return err
+	}
+
+	var version int
+	err = db.conn.QueryRow("SELECT value FROM metadata WHERE key = 'schema_version'").Scan(&version)
+	if err != nil && err != sql.ErrNoRows {
+		return err
+	}
+
+	if version < currentSchemaVersion {
+		log.Printf("initializing schema version %d", currentSchemaVersion)
+		_, err = db.conn.Exec("INSERT OR REPLACE INTO metadata (key, value) VALUES ('schema_version', ?)", currentSchemaVersion)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (db *DB) GetThumbnail(path string) (image.Image, bool) {
+	var data []byte
+	err := db.conn.QueryRow("SELECT data FROM thumbnails WHERE path = ?", path).Scan(&data)
+	if err != nil {
+		if err != sql.ErrNoRows {
+			log.Printf("error getting thumbnail from DB for %s: %v", path, err)
+		}
+		return nil, false
+	}
+
+	img, err := jpeg.Decode(bytes.NewReader(data))
+	if err != nil {
+		log.Printf("error decoding thumbnail for %s: %v", path, err)
+	}
+
+	return img, true
+}
+
+func (db *DB) SetThumbnail(path string, img image.Image) {
+	var buf bytes.Buffer
+	if err := jpeg.Encode(&buf, img, nil); err != nil {
+		log.Printf("error encoding thumbnail for %s: %v", path, err)
+		return
+	}
+
+	_, err := db.conn.Exec("INSERT OR REPLACE INTO thumbnails (path, data) VALUES (?, ?)", path, buf.Bytes())
+	if err != nil {
+		log.Printf("error setting thumbnail in DB for %s: %v", path, err)
+	}
+}
